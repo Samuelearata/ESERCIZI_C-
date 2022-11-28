4cf15855599ec9b8a81bde1b9d949e4c318e3952
Samuelearata <Samuele.arata.002@gmail.com>
2022-11-28 17:26:32 +0100

    fine

---
+++ Cap3_cicli/Struct/CAMEL.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+struct Data{
+int g;
+int m;
+int a;
+};
+int main ()
+{
+data unaData;
+
+std::cout <<"scrivi un giorno:";
+std:cin >> unaData.g;
+std::cout <<"scrivi un mese:";
+std:cin >> unaData.m;
+std::cout <<"scrivi un anno:";
+std:cin >> unaData.a;
+
+std::cout <<"hai scritto la data";
+std:cout >> unaData.g <<"/";
+std:cout >> unaData.m<<"/";
+std:cout >> unaData.a <<"/";
+}
---
+++ esercizi parte due /2.2/2.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int num;
+    cout << "inserire il valore" <<endl;
+    cin >> num;
+    if (num<0)
+       cout << -num << endl;
+        else
+        cout << num << endl; 
+    
+    return 0;
+}
