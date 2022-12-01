10bee41ec3c7eec2756398af49459653950aa827
Samuelearata <Samuele.arata.002@gmail.com>
2022-12-01 16:15:16 +0100

    anno bisestile non ne ho voglia

---
+++ 2.12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main {
+    int anno;
+    cout <<"inserire un anno"<<endl;
+    cin >> anno;
+    if (anno % 4 = 0)
+        cout << anno<< "è un anno bisestile"
+}
