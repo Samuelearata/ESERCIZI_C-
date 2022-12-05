f9297ec08dcf9bc345651471810f8e7279aa4ba5
Samuelearata <Samuele.arata.002@gmail.com>
2022-12-05 23:23:36 +0100

    save

---
+++ esercizi_cicli/3.1/3.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main {
+    float Lett
+    cout <<"inserire una lettera maiuscola"<<endl;
+    cin<<Lett;
+    int i;
+    while lett<A || lett>Z
+        Cout << "inserisci una lettera maiuscola"<<endl;
+
+
+    
+
+
+}
